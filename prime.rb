470ced04fef564e7aec306ac4352ea3324957b2b
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-27 22:41:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
-def prime?(n)
-  new_arr = [*2..(n-1)]
-  if n >=0
-    !new_arr.any? {|element| n % element == 0}
-
+def prime_number(n)
+  if n <= 0 
+    FALSE
   else
-    false
+    new_arr = [*2..(n-1)]
+    !new_arr.any? {|element| n % element == 0}
   end
 end
